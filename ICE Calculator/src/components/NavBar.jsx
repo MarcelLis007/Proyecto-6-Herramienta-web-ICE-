9682d5a21bfc010a7d86bf3944e79ac9ad64a977
MarcelLis007 <marcelacm26@uniandes.edu.ec>
2025-01-27 02:02:52 -0500

    sd

---
+++ ICE Calculator/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between items-center bg-white shadow-md p-4">
+            <div className="flex items-center">
+                <img src="logo.svg" alt="logo" className="h-10" />
+            </div>
+            <div className="flex space-x-6">
+                <a href="#" className="text-gray-700 hover:text-blue-600">Home</a>
+                <a href="#" className="text-gray-700 hover:text-blue-600">About</a>
+                <a href="#" className="text-gray-700 hover:text-blue-600">Contact</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ ICE Calculator/src/pages/landingPage.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/NavBar";
+const LandingPage = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
+
+export default (LandingPage);
